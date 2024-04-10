3a30824e789e446554d4f6203822108f3a4f47c4
cmj <cmj@cmj.tw>
2024-04-10 21:33:56 +0800

    feat(svc|ssh): allow execute the remote command from SSH

--- pkg/service/ssh/service.go
+++ pkg/service/ssh/service.go
@@ -136,6 +136,7 @@ func (s *SSH) handleConn(conn net.Conn, config *ssh.ServerConfig) {
 	// Service the incoming Channel channel.
 	for newChannel := range chans {
 		// handle the channel in another goroutine
+		log.Info().Str("service", SVC_NAME).Interface("channel", newChannel).Msg("new channel")
 		go s.handleChannel(newChannel)
 	}
 
@@ -144,13 +145,65 @@ func (s *SSH) handleConn(conn net.Conn, config *ssh.ServerConfig) {
 
 // Handle the incoming SSH channel
 func (s SSH) handleChannel(newChannel ssh.NewChannel) {
+	defer log.Info().Str("service", SVC_NAME).Msg("closing the channel ...")
+
 	// Accept the channel
-	channel, _, err := newChannel.Accept()
+	channel, reqs, err := newChannel.Accept()
 	if err != nil {
 		log.Error().Err(err).Msg("failed to accept the channel")
 		return
 	}
 	defer channel.Close()
+
+	for req := range reqs {
+		log.Info().Str("service", SVC_NAME).Str("type", req.Type).Msg("handling the request ...")
+
+		switch req.Type {
+		case "env":
+			// ignore the environment request
+			if req.WantReply {
+				req.Reply(true, nil)
+			}
+			log.Info().Str("service", SVC_NAME).Str("type", req.Type).Msg("ignoring the request")
+		case "subsystem":
+			// reject the subsystem request
+			if req.WantReply {
+				req.Reply(false, nil)
+			}
+			log.Info().Str("service", SVC_NAME).Str("type", req.Type).Msg("rejecting the request")
+		case "exec":
+			command := string(req.Payload[4:])
+			result := s.handleCommand(command)
+
+			// reply the command result
+			if req.WantReply {
+				req.Reply(true, nil)
+			}
+			channel.Write([]byte(result))
+			log.Info().Str("service", SVC_NAME).Str("type", req.Type).Str("command", command).Msg("executing the command")
+
+			// close the channel
+			return
+		default:
+			log.Warn().Str("service", SVC_NAME).Interface("req", req).Msg("unsupported request")
+			req.Reply(false, nil)
+		}
+	}
+}
+
+func (s *SSH) handleCommand(command string) string {
+	var result []string
+
+	commands := strings.Split(command, ";")
+	for _, cmd := range commands {
+		cmd = strings.TrimSpace(cmd)
+		switch cmd {
+		default:
+			result = append(result, fmt.Sprintf("bash: %s: command not found", command))
+		}
+	}
+
+	return strings.Join(result, "\n")
 }
 
 func (s *SSH) sendAuthMessage(username string, password string, remote string) {
