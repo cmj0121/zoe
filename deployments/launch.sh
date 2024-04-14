c0949676fb6195bf1c0de7e4a582ece4b4deb749
cmj <cmj@cmj.tw>
2024-04-14 21:10:59 +0800

    build(docker): downgrade the User permission

--- deployments/launch.sh
+++ deployments/launch.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 set -e
 
-mkdir -p /svc/zoe
+mkdir -p /tmp/zoe
 
-zoe -vv migrate sqlite3:///svc/zoe/records.db
+zoe -vv migrate sqlite3:///tmp/zoe/records.db
 zoe -vv launch --config zoe.yaml
